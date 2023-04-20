a095830626b61ef64f51f2eb855ff190d26f7d41
hanhatnheo <phamjudy2004@gmail.com>
2023-04-19 20:15:15 -0400

    About Us

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,8 +1,30 @@
 import { ContentSection, ImageBackground, PageContainer } from "../components/ContentBlocks";
+import awards from "../assets/awards.png";
+import './aboutus.css'
 
 const AboutUs = () => {
     return <PageContainer>
-        Hi!
+        <ContentSection centered textSize="medium">
+        <h1>
+            About Us
+        </h1>
+        <p>
+            SafeStamp Inc. is an award-winning company with an award-winning product on a
+        mission to end counterfeiting.
+        </p>
+        <p>
+            <img src={awards}/>
+        </p>
+        <p>
+            SafeStamp Inc. is a veteran-owned firm with a mission to eliminate counterfeit pharmaceuticals by providing an innovative, end-user friendly, and irreplicable deep tech indicator for the product packaging. The indicator, which changes colors and reveals patterns when touched, allows users to easily distinguish legitimate products from counterfeits. It resists replication by counterfeiters through the employment of advanced nanomaterials; thus, it can serve as proof of authenticity for its use. 
+        </p>
+        <p> 
+            We work with our clients to customize our patented, advance nanomaterial to their anticounterfeiting and branding needs. We can manufacture tailor-made designs that stand out against competitors, engage end-consumers, and can be placed on all forms of packaging. 
+        </p>
+        <p>
+            We manufacture in America but serve clients and partner with governments throughout the globe.
+        </p>
+        </ContentSection>
     </PageContainer>
 
 };
